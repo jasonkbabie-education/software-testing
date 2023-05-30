edb9a81475ee86e6ecd09c57fd5e9cbcb0a60f14
Jason K. Babie <jbabie@imxsoftware.com>
2023-05-30 11:06:01 +0200

    Implement Customer module and corresponding data layer API

---
+++ src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.amigoscode.testing.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+    @Test
+    void itShouldSelectCustomerByPhoneNumber() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void itShouldSaveCustomer() {
+        //Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Jason", "00000");
+        //When
+        underTest.save(customer);
+        //Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualTo(id);
+//                    assertThat(c.getName()).isEqualTo("Jason");
+//                    assertThat(c.getPhoneNumber()).isEqualTo("00080");
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+}
